1797b0b3aebdb96f1f1d70d3d4fd5202ea6b4c8f
Mark Thorhauge <mark.thorhauge@keylane.com>
2018-10-12 22:43:16 +0200

    Adds mapGenerator

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,10 +1,12 @@
 define(function (require) {
     var engine = require('app/engine');
     var ui = require('app/ui');
+    var mapGenerator = require('app/mapGenerator');
 
     engine.initialize();
     ui.initialize();
 
     window.engine = engine;
     window.ui = ui;
+    window.mapGenerator = mapGenerator;
 });
