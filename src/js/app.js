3343e67fb6872cedb7a446668746117d4b3971ff
Mathias <m@thias.dk>
2018-10-19 21:57:10 +0200

    Changed to full jQuery build from slim

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl: 'js/libs',
     paths: {
         app: "../app",
-        jquery: "jquery-3.3.1.slim.min",
+        jquery: "jquery-3.3.1.min",
         handlebars: "handlebars-v4.0.12"
     }
 });
