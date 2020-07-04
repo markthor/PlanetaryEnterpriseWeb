d85ea1c07fc43985e06f6f88d72bea4bcf1cd12e
Mark Thorhauge <markthorh@gmail.com>
2020-07-05 01:36:57 +0200

    Revert "Changed icon"
    This reverts commit a2ac8414dfbfa6f41158fda099ecdc1a4e9aa769.

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,7 @@ requirejs.config({
     paths: {
         app: "../app",
         jquery: "jquery-3.3.1.min",
-        handlebars: "handlebars-v4.0.12",
-        plotly: "plotly-latest.min"
+        handlebars: "handlebars-v4.0.12"
     }
 });
 
