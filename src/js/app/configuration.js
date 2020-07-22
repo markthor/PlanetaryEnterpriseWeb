46686679e686ca51f43c891792a786319ccd965f
unknown <markthorh@gmail.com>
2020-07-22 16:35:22 +0200

    seed money now continues through whole game

--- src/js/app/configuration.js
+++ src/js/app/configuration.js
@@ -1,6 +1,6 @@
 define(["jquery"], function($) {
     return {
-        numberOfRoundsWithAdditionalIncome: 6,
+        numberOfRoundsWithAdditionalIncome: 99999,
         additionalIncome: 25
     }
 });
